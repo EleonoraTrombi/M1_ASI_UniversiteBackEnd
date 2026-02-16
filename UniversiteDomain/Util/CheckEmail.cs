2fd86872ee55395d570b91c6cdca26792408ee95
EleonoraTrombi <eleonora.trombi@etud.u-picardie.fr>
2026-02-16 11:34:27 +0100

    Update CheckEmail.cs

--- UniversiteDomain/Util/CheckEmail.cs
+++ UniversiteDomain/Util/CheckEmail.cs
@@ -11,17 +11,13 @@ public class CheckEmail
             return false;
         try
         {
-            // Normalize the domain
             email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
                 RegexOptions.None, TimeSpan.FromMilliseconds(200));
 
-            // Examines the domain part of the email and normalizes it.
             string DomainMapper(Match match)
             {
-                // Use IdnMapping class to convert Unicode domain names.
                 var idn = new IdnMapping();
 
-                // Process domain name (throws ArgumentException on invalid)
                 string domainName = idn.GetAscii(match.Groups[2].Value);
 
                 return match.Groups[1].Value + domainName;
